af71606cfb9603b127779cf61e6f53b71401b78e
Debasish Sahoo <debasish.sahoo@nttdata.com>
2024-05-23 15:09:41 -0400

    Created example for private apim

--- examples/private/versions.tf
+++ examples/private/versions.tf
@@ -10,4 +10,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+terraform {
+  required_version = ">= 1.5.0, <= 1.5.5"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.67"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
